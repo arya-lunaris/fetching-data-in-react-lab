28143836c823295a2f840e3a0915807469abc2ce
Arya Ram <arya_ram@outlook.com>
2025-01-30 17:48:01 +0000

    Form and search bar appears on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,73 @@
-// src/App.jsx
+import StarshipSearch from './components/StarshipSearch/StarshipSearch.jsx'
+import StarshipList from './components/StarshipList/StarshipList.jsx'
+import { useState, useEffect } from 'react'
+import { getStarships } from './services/starShipService.js'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [starshipsData, setStarshipsData] = useState([])
+  const [displayedStarships, setDisplayedStarships] = useState([])
+  const [error, setError] = useState('')
+
+
+  useEffect(() => {
+    const getInitialData = async () => {
+      try {
+        const response = await getStarshipsData('')
+        setStarshipsData({
+          name: response.data.name,
+          starship_class: response.data.starship_class,
+          manufacturer: response.data.manufacturer,
+          model: response.data.model
+        })
+        setDisplayedStarships({
+          name: response.data.name,
+          starship_class: response.data.starship_class,
+          manufacturer: response.data.manufacturer,
+          model: response.data.model
+        })
+
+      } catch (error) {
+        // console.error("Error fetching starships:");
+        // setError();
+      }
+    }
+    getInitialData()
+  }, [])
+
+
+  const getStarshipsData = async () => {
+    try {
+      const response = await getStarshipsData(searchTerm)
+      setStarshipsData({
+        name: response.data.name,
+        starship_class: response.data.starship_class,
+        manufacturer: response.data.manufacturer,
+        model: response.data.model
+      })
+
+    } catch (error) {
+      // console.error("Error fetching starships:");
+      // setError();
+    }
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Star Wars API</h1>
+      <form onSubmit={getStarshipsData}>
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Enter starship name"
+          value={searchTerm}
+          onChange={(error) => setSearchTerm(error.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ src/services/starShipService.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://swapi.info/api/starships';
+
+export const getStarships = (searchTerm) => {
+  return axios.get(`${BASE_URL}/${searchTerm}`)
+};
