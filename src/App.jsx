bdcb0bfeda9cda3bb5a01cf624e7d134859bfd27
Arya Ram <arya_ram@outlook.com>
2025-01-31 11:13:38 +0000

    Finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -6,68 +6,57 @@ import { getStarships } from './services/starShipService.js'
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [starshipsData, setStarshipsData] = useState([])
-  const [displayedStarships, setDisplayedStarships] = useState([])
   const [error, setError] = useState('')
-
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const getInitialData = async () => {
       try {
-        const response = await getStarshipsData('')
-        setStarshipsData({
-          name: response.data.name,
-          starship_class: response.data.starship_class,
-          manufacturer: response.data.manufacturer,
-          model: response.data.model
-        })
-        setDisplayedStarships({
-          name: response.data.name,
-          starship_class: response.data.starship_class,
-          manufacturer: response.data.manufacturer,
-          model: response.data.model
-        })
+        const response = await getStarships()
+        console.log(response)
+        setStarshipsData(response.data.results)
 
       } catch (error) {
-        // console.error("Error fetching starships:");
-        // setError();
+        console.error("Error fetching starships:", error);
+        setError();
       }
     }
     getInitialData()
   }, [])
 
-
-  const getStarshipsData = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
     try {
-      const response = await getStarshipsData(searchTerm)
-      setStarshipsData({
-        name: response.data.name,
-        starship_class: response.data.starship_class,
-        manufacturer: response.data.manufacturer,
-        model: response.data.model
-      })
-
+      const response = await getStarships(searchTerm)
+      console.log(response)
+      setStarshipsData(response.data.results)
+      setSearchTerm("")
     } catch (error) {
-      // console.error("Error fetching starships:");
-      // setError();
+      console.error("Error fetching starships:", error);
+      setError();
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
       <h1>Star Wars API</h1>
-      <form onSubmit={getStarshipsData}>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           id="search"
           name="search"
           placeholder="Enter starship name"
           value={searchTerm}
-          onChange={(error) => setSearchTerm(error.target.value)}
+          onChange={(event) => setSearchTerm(event.target.value)}
         />
         <button type="submit">Search</button>
+        <button onClick={() => useEffect}>Reset</button>
       </form>
+      <StarshipList starships={starshipsData} />
     </>
   );
 }
-
 export default App;
